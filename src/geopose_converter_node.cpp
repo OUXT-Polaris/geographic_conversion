0e11b355f424121ee546a7dd23148d745277e2db
Masaya Kataoka <ms.kataoka@gmail.com>
2019-06-19 09:17:59 +0900

    add geopose_converter_node

---
+++ src/geopose_converter_node.cpp
@@ -0,0 +1,14 @@
+#include <geographic_conversion/geopose_converter.h>
+
+// headers in ros
+#include <ros/ros.h>
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "geopose_converter_node");
+    ros::NodeHandle nh;
+    ros::NodeHandle pnh("~");
+    //geographic_conversion::FixConverter converter(nh,pnh);
+    ros::spin();
+    return 0;
+}
