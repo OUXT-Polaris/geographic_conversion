c944b84c950e1c163bfa5495d3e195727937cfbf
Masaya Kataoka <ms.kataoka@gmail.com>
2021-03-28 14:35:52 +0900

    enable pass cpplint
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- include/geographic_conversion/fix_converter_component.hpp
+++ include/geographic_conversion/fix_converter_component.hpp
@@ -16,16 +16,16 @@
 #define GEOGRAPHIC_CONVERSION__FIX_CONVERTER_COMPONENT_HPP_
 
 // Headers in ROS
-#include <geodesy/utm.h>
 #include <geometry_msgs/msg/point_stamped.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/nav_sat_fix.hpp>
+#include <geodesy/utm.h>
+#include <ctype.h>
 
 // Headers in STL
 #include <string>
 #include <limits>
 #include <cmath>
-#include <ctype.h>
 
 #if __cplusplus
 extern "C" {
